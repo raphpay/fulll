2297bcb5c565162b70df5bcbf5f471fcd71843a8
raphpay <raphpay.dev@gmail.com>
2024-03-09 15:46:02 +0100

    Feature: Implement UI #1

--- App.tsx
+++ App.tsx
@@ -1,13 +1,61 @@
-import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import styles from './src/ui/assets/styles/AppStyles';
 
 function App(): React.JSX.Element {
+  const docIcon = require('./src/ui/assets/images/doc.on.doc.png');
+  const trashIcon = require('./src/ui/assets/images/trash.png');
+  const checkmarkFill = require('./src/ui/assets/images/checkmark.square.fill.png');
+  const checkmarkEmpty = require('./src/ui/assets/images/square.png');
+
+  const [searchText, setSearchText] = useState<string>('');
+  const [numberOfItemsSelected, setNumberOfItemsSelected] = useState<number>(0);
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Github Search</Text>
       </View>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          value={searchText}
+          onChangeText={setSearchText}
+          placeholder={'Search Input'}
+          style={styles.searchInput}
+        />
+      </View>
+      <View style={styles.scrollViewHeader}>
+        <Text>{numberOfItemsSelected} elements selected</Text>
+        <View style={styles.iconContainer}>
+          <TouchableOpacity>
+            <Image source={docIcon} style={styles.smallIcon} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={trashIcon} style={styles.smallIcon} />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.listItem}>
+          <Image source={checkmarkEmpty} style={styles.checkmarkIcon} />
+          <View style={styles.avatarContainer}>
+            <View style={styles.avatar} />
+            <Text>ID</Text>
+            <Text>Login</Text>
+          </View>
+          <TouchableOpacity style={styles.profileButton}>
+            <Text>View Profile</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
--- src/ui/assets/styles/AppStyles.ts
+++ src/ui/assets/styles/AppStyles.ts
@@ -16,6 +16,73 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
+  textInputContainer: {
+    width: '100%',
+    paddingHorizontal: 16,
+    paddingVertical: 24,
+  },
+  searchInput: {
+    height: 55,
+    width: '100%',
+    backgroundColor: '#d6d6d6',
+    borderRadius: 10,
+  },
+  scrollViewHeader: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+  },
+  smallIcon: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+  },
+  scrollView: {
+    paddingTop: 16,
+    paddingHorizontal: 16,
+    width: '100%',
+  },
+  listItem: {
+    backgroundColor: '#d6d6d6',
+    borderRadius: 20,
+    width: '100%',
+    height: 200,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 8,
+  },
+  avatarContainer: {
+    alignItems: 'center',
+  },
+  avatar: {
+    backgroundColor: '#3c8681',
+    width: 55,
+    height: 55,
+    borderRadius: 55,
+  },
+  emptyView: {
+    flex: 1,
+  },
+  checkmarkIcon: {
+    width: 15,
+    height: 15,
+    resizeMode: 'contain',
+    position: 'absolute',
+    top: 15,
+    left: 10,
+  },
+  profileButton: {
+    backgroundColor: '#479ff8',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 55,
+    width: '50%',
+  },
 });
 
 export default styles;
