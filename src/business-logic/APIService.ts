c27ede41e5e1076145f38e552865d66bc3e3f66f
raphpay <raphpay.dev@gmail.com>
2024-03-10 11:14:19 +0100

    Feature: Add comments #1

--- src/business-logic/APIService.ts
+++ src/business-logic/APIService.ts
@@ -1,6 +1,12 @@
 import IUser, { IGithubResponse } from './model/IUser';
 
 class APIService {
+  /**
+   * Fetches users from the GitHub API based on the provided endpoint.
+   * @param endpoint The search query or endpoint to fetch users from.
+   * @returns A Promise that resolves to an array of IUser objects representing the fetched users.
+   * @throws Error if the API call fails or if the API rate limit is exceeded.
+   */
   static async getUsers(endpoint: string): Promise<IUser[]> {
     try {
       const url = `https://api.github.com/search/users?q=${endpoint}`;
@@ -30,6 +36,11 @@ class APIService {
     }
   }
 
+  /**
+   * Extracts user data from the GitHub API response.
+   * @param response The response object from the GitHub API.
+   * @returns An array of IUser objects extracted from the response.
+   */
   private static extractUsers(response: IGithubResponse): IUser[] {
     if (!response || !response.items || !Array.isArray(response.items)) {
       return [];
---
+++ src/ui/screens/ListScreen.tsx
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import {
+  Alert,
+  FlatList,
+  Image,
+  SafeAreaView,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+import APIService from '../../business-logic/APIService';
+import IUser from '../../business-logic/model/IUser';
+
+import ContentUnavailableView from '../components/ContentUnavailableView';
+import IconButton from '../components/IconButton';
+import ListItem from '../components/ListItem';
+
+import styles from '../assets/styles/screens/ListScreenStyles';
+
+function ListScreen(): React.JSX.Element {
+  const docIcon = require('../assets/images/doc.on.doc.png');
+  const trashIcon = require('../assets/images/trash.png');
+  const peopleIcon = require('../assets/images/person.2.fill.png');
+  const editIcon = require('../assets/images/square.and.pencil.png');
+
+  const [searchText, setSearchText] = useState<string>('');
+  const [numberOfItemsSelected, setNumberOfItemsSelected] = useState<number>(0);
+  const [users, setUsers] = useState<IUser[]>([]);
+  const [selectedUsers, setSelectedUsers] = useState<IUser[]>([]);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  async function onChangeText(text: string) {
+    setSearchText(text);
+    if (text.length === 0) {
+      setUsers([]);
+    } else {
+      try {
+        const apiUsers = await APIService.getUsers(text);
+        setUsers(apiUsers);
+      } catch (error) {
+        const castedError = error as Error;
+        Alert.alert('An error occured', castedError.message);
+      }
+    }
+  }
+
+  function duplicateItems() {
+    const originalArray = selectedUsers;
+    const selectedDuplicatesArray = originalArray.concat(originalArray);
+    const duplicatedArray = [...originalArray, ...users];
+    setSelectedUsers(selectedDuplicatesArray);
+    setNumberOfItemsSelected(selectedUsers.length);
+    setUsers(duplicatedArray);
+  }
+
+  function removeItems() {
+    const originalArray = users;
+    for (const userToRemove of selectedUsers) {
+      const indexToRemove = originalArray.indexOf(userToRemove);
+      originalArray.splice(indexToRemove, 1);
+    }
+    setSelectedUsers([]);
+    setNumberOfItemsSelected(selectedUsers.length);
+    setUsers(originalArray);
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Github Search</Text>
+      </View>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          value={searchText}
+          onChangeText={onChangeText}
+          placeholder={'Search Input'}
+          style={styles.searchInput}
+        />
+      </View>
+      <View style={styles.scrollViewHeader}>
+        <Text>{numberOfItemsSelected} elements selected</Text>
+        <View style={styles.iconContainer}>
+          {isEditing && (
+            <>
+              <IconButton iconSource={docIcon} action={duplicateItems} />
+              <IconButton iconSource={trashIcon} action={removeItems} />
+            </>
+          )}
+
+          <IconButton
+            iconSource={editIcon}
+            action={() => setIsEditing(!isEditing)}
+          />
+        </View>
+      </View>
+      {users.length === 0 ? (
+        <ContentUnavailableView
+          title={'No user searched'}
+          message={'Search for a user'}
+          image={<Image source={peopleIcon} />}
+        />
+      ) : (
+        <FlatList
+          style={styles.scrollView}
+          data={users}
+          renderItem={renderItem => (
+            <ListItem
+              user={renderItem.item}
+              selectedUsers={selectedUsers}
+              setSelectedUsers={setSelectedUsers}
+              numberOfItemsSelected={numberOfItemsSelected}
+              setNumberOfItemsSelected={setNumberOfItemsSelected}
+              isEditing={isEditing}
+            />
+          )}
+          keyExtractor={item => item.id}
+        />
+      )}
+    </SafeAreaView>
+  );
+}
+
+export default ListScreen;
